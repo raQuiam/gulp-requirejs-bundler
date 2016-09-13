180191ff9cecfd99647b77da1f403009bd2c66d4
Vashira <r.vashira@gmail.com>
2016-09-13 12:46:49 +0700

    Append baseUrl in bundleConfigCode.

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ module.exports = function(options) {
                     return [bundleOutput.itemName, bundleOutput.modules]
                 })),
                 bundleConfigCode = '\nrequire.config('
-                    + JSON.stringify({ bundles: bundleConfig }, true, 2)
+                    + JSON.stringify({ bundles: bundleConfig, baseUrl: options.bundleUrl }, true, 2)
                     + ');\n';
             return new File({
                 path: primaryOutput.file.path,
